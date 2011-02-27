1f4d9142503ebb86c65667da8edd23d17ec1a898
Robb Broome <robb@teamvoice.com>
2011-02-27 16:09:06 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neotest::Application.config.secret_token = '68e60a9decc0ecd2474a86d0bfa7edd44c6891a147ae5122b239cbd5c1894382b5f56b3b61cbf01c87cd01d389670216f3773b79e1eae3d010b757a608ebc53c'
